a6ac1be3506c0108d3fa3e299da5e62a4183f974
yishuan <edward0487@gmail.com>
2017-05-25 16:56:39 +0800

    P2

--- app/src/main/java/com/s10576019/cyel/githubtest5/Main2Activity.java
+++ app/src/main/java/com/s10576019/cyel/githubtest5/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.s10576019.cyel.githubtest5;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void clickButtonJumpPage3(View view) {
+        Intent intent = new Intent(this, Main3Activity.class);
+        startActivity(intent);
     }
 }
